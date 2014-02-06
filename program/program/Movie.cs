2bf7a5586c47b5a2386d79c693769c5eee4e364b
Bruno Cadorette <brunocadorette@outlook.com>
2014-02-06 18:37:52 -0500

    Hi @francois, I hope this is okay Sample: shawshank Title: The Shawshank Redemption Year: 1994
    gain
    Title: Pain & Gain
    Year: 2013
    
    hobbit
    Title: The Hobbit: There and Back Again
    Year: 2014
    
    lego
    Title: The Lego Movie
    Year: 2014

---
+++ program/program/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class Movie
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ program/program/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string api = "http://deanclatworthy.com/imdb/";
+            if (args.Length > 0)
+            {
+                if (IsOnline(api))
+                {
+                    var webClient = new WebClient();
+                    string title = args[0].Replace(' ', '+');
+                    string json = webClient.DownloadString(String.Format("{0}?q={1}", api, title));
+                    var movie = JsonConvert.DeserializeObject<Movie>(json);
+                    Console.WriteLine("Title: " + movie.Title);
+                    Console.WriteLine("Year: " + movie.Year);
+                }
+                else
+                {
+                    Console.WriteLine("Connection with the server is impossible, please wait a couple of hours and try again");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter at least one argument");
+            }
+        }
+        static bool IsOnline(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+            request.Method = "HEAD";
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                return (response.StatusCode == HttpStatusCode.OK);
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+    }
+}
