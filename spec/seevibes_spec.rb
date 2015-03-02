ae6d79cc0ce561794b85d195ff9f9f2b36f3b4ea
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2015-03-02 12:04:47 -0500

    create ImdbSearcher class, pass test for returning parsed results array.

--- spec/seevibes_spec.rb
+++ spec/seevibes_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require_relative '../lib/movie'
+require_relative '../lib/imdb_searcher'
 
 describe 'Movie#to_s' do
   it "returns the title and year on two lines" do
@@ -7,3 +8,16 @@ describe 'Movie#to_s' do
     expect("#{movie}").to eq "Title: Fargo\nYear: 1996"
   end
 end
+
+describe 'ImdbSearcher#fetch_results' do
+  it "returns an array of movie results" do
+    search = ImdbSearcher.new("Lost Weekend")
+    expect(search.fetch_results).to be_an(Array)
+  end
+
+  it "parses movie results to hashes with title and year keys" do
+    search = ImdbSearcher.new("When Harry Met Sal")
+    expect( (search.fetch_results).all? {|result| result.has_key?(:title) && result.has_key?(:year) } ).to be true
+  end
+end
+
