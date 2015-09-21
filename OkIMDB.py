9d0645105e9ac20cddd55225acfff235af877668
KerbKerb <kerby.geffrard@gmail.com>
2015-09-21 15:30:23 -0400

    Patron

---
+++ OkIMDB.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+__author__ = 'KerbKerb'
+
+
+def search_engine(name):
+    title = None
+    year = None
+    movies = []
+
+    # TODO: Talk with the api and the keyword given
+    # TODO: All the awnsers in the movies list
+    # TODO: Sort all the awnsers
+
+    for movie in movies:
+        yield (title, year)
+
+    return None
+
+
+if __name__ == "__main__":
+    # TODO: Only one arg? Cool otherwise show usage
+    pass
