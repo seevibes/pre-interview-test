bc3b029cd994de303fec8b52b9ac3ff556bf22d8
Samuel Laroche <samuel@jfzone.net>
2016-10-13 21:05:30 -0400

    add comments

--- program.rb
+++ program.rb
@@ -4,8 +4,19 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-uri = URI('http://www.omdbapi.com/?s=' + ARGV[0] +'&y=&plot=short&r=json')
-res = Net::HTTP.get(uri)
-json = JSON.parse(res)
-puts "Title: " + json["Search"][0]["Title"]
-puts "Year: " +json["Search"][0]["Year"]
+name = ARGV[0]
+
+# if there is no movie in the entry, the error message show up
+if (!name.nil?)
+	# the uri to get the movie information from the open movie data base api https://www.omdbapi.com/
+	# the parameter s fetch a list of movies associte to the name
+	uri = URI('http://www.omdbapi.com/?s=' + name +'&y=&r=json')
+	res = Net::HTTP.get(uri)
+	# Parsing the response in a json object
+	json = JSON.parse(res)
+	# I take the first movie from the list of movies
+	puts "Title: " + json["Search"][0]["Title"]
+	puts "Year: " +json["Search"][0]["Year"]
+else
+	puts "no argument"
+end
--- readme.md
+++ readme.md
@@ -1,11 +1,12 @@
-Pour executer le programme :
-ruby program.rb argument
-
-Bonjour, 
-voici un petit programme qui va cherhcer le titre et l'année d'un film 
+voici un petit programme qui va cherhcer le titre et l'année d'un film
 avec le API de imdb (www.omdbapi.com)
 
+Pour executer le programme :
+./program.rb arg
+ou
+ruby program.rb arg
+
 Samuel Laroche LARS14039507
-samuel.laroche@etsmtl.net
+samuel.laroche.1@etsmtl.net
 Étudiant à l'ETS en génie logiciel
 stage 2
